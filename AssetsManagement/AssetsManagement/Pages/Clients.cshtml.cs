ba49e78b82b5589eb630bcfd402bf9419f9c75fb
maria-beck <mariab@easymarkets.com>
2025-06-25 11:01:58 +0300

    .

--- AssetsManagement/AssetsManagement/Pages/Clients.cshtml.cs
+++ AssetsManagement/AssetsManagement/Pages/Clients.cshtml.cs
@@ -2,12 +2,14 @@ using AssetsManagement.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
+
 namespace AssetsManagement.Pages
 {
     public class ClientsModel : PageModel
     {
         public List<Client> Clients { get; set; }
         public List<Asset> Assets { get; set; }
+        public int? SelectedClientId { get; set; }
         private Db db;
 
         public ClientsModel(Db db)
@@ -20,6 +22,12 @@ namespace AssetsManagement.Pages
             Clients = db.Clients;
             if(id != null)
             {
+                var selectedClient = Clients.FirstOrDefault(c => c.Id == id);
+                if(selectedClient != null)
+                {
+                    Assets = selectedClient.Assets;
+                    SelectedClientId = selectedClient.Id;
+                }
 
             }
         }
