ae02c42b0b67d2a61a1c86b43ae12f6e3cf481e3
maria-beck <mariab@easymarkets.com>
2025-07-03 15:32:29 +0300

    Create AccountManagers.cshtml.cs

---
+++ AssetsManagement/AssetsManagement/Pages/AccountManagers.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace AssetsManagement.Pages
+{
+    public class AccountManagersModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
